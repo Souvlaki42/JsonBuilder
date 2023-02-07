0f35c66767442c8c5af048e69be0fda80d3060ad
Souvlaki42 <souvlaki420@gmail.com>
2023-02-07 20:09:43 +0200

    error: return null

--- errorHandler.py
+++ errorHandler.py
@@ -6,15 +6,16 @@ def handlePossibeErrors(lambdaFunctionToExecute, printDebuggingMessage = False, 
 	try:
 		return lambdaFunctionToExecute()
 	except Exception as e:
-			print(f"Error: {e}")
-			if detailedLog:
-				exception_type, exception_object, exception_traceback = sys.exc_info()
-				filename = exception_traceback.tb_frame.f_code.co_filename
-				line_number = exception_traceback.tb_lineno
+		print(f"Error: {e}")
+		if detailedLog:
+			exception_type, exception_object, exception_traceback = sys.exc_info()
+			filename = exception_traceback.tb_frame.f_code.co_filename
+			line_number = exception_traceback.tb_lineno
 
-				print(f"Type: {exception_type}")
-				print(f"File name: {filename}")
-				print(f"Line number: {line_number}")
+			print(f"Type: {exception_type}")
+			print(f"File name: {filename}")
+			print(f"Line number: {line_number}")
+		return None
 	finally:
 		if printDebuggingMessage: timerStopping = round(timer() * 1000)
 		if printDebuggingMessage: print(f"JSON executed in {timerStopping-timerStarting} ms")
